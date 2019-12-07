8bb4610b187a6a078a2bb4ec49026b74c1ea400b
PaladiRka <milakins@bk.ru>
2019-12-07 18:57:45 +0300

    Fix child.

--- lab7/lab7.c
+++ lab7/lab7.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <sys/wait.h>
 
+#define __USE_GNU
+#include <unistd.h>
+
+#include <sys/wait.h>
 #include <fcntl.h>
 
 
@@ -13,11 +15,13 @@ int main(int argc, char* argv[])
         printf("Ошибка. Программа должна принимать 3 аргумента\n");
     }
 
-    int fildes[2];
-    pipe(fildes);
+    int pipedes[2];
+    __pid_t child_id_1;
+    __pid_t child_id_2;
+    pipe2(pipedes, O_NONBLOCK);
     FILE *fout;
     fout = fopen(argv[3], "w");
-    if(fork() == 0) 
+    if((child_id_1 = fork()) == 0) 
     {
         FILE *fin1 = fopen(argv[1], "r");
         char ch;
@@ -25,14 +29,14 @@ int main(int argc, char* argv[])
         {
             ch = fgetc(fin1);
             if (ch != EOF)
-                write(fildes[1], &ch, 1);
+                write(pipedes[1], &ch, 1);
         }
-
         fclose(fin1);
+        _exit(0);
     }
     else
     {
-        if (fork() == 0)
+        if ((child_id_2 = fork()) == 0)
         {
             FILE *fin2 = fopen(argv[2], "r");
             char ch;
@@ -41,27 +45,33 @@ int main(int argc, char* argv[])
                 ch = fgetc(fin2);
                 if (ch != EOF)
 
-                write(fildes[1], &ch, 1);
+                write(pipedes[1], &ch, 1);
             }
             fclose(fin2);
+            _exit(0);
         }
-
     }
 
-    fcntl(fildes[0], F_SETFL, O_NONBLOCK);
+    // fcntl(pipedes[0], F_SETFL, O_NONBLOCK);
     char ch;
     int buff;
-    while ((buff = read(fildes[0], &ch, sizeof(ch))) > 0)
+
+    waitpid(child_id_1, NULL, 0);
+    waitpid(child_id_2, NULL, 0);
+
+    while ((buff = read(pipedes[0], &ch, sizeof(ch))) > 0)
     {
         printf("%c", ch);
         fprintf(fout, "%c", ch);
     }
 
+    printf("\n");
+
     int status;
     wait(&status);
     wait(&status);
     fclose(fout);
-    close(fildes[0]);
-    close(fildes[1]);
+    close(pipedes[0]);
+    close(pipedes[1]);
     return 0;
 }
