b468e3d1e1e4e5b81e8b030edb564fd2889019e9
Yaroslav Atroshenko <yaaroslavok@gmail.com>
2019-12-08 22:54:42 +0300

    add malloc for threads

--- lab4/lab4_2.c
+++ lab4/lab4_2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <stdlib.h>
 
 struct thread_inputs {
     char* input_line;
@@ -8,7 +9,7 @@ struct thread_inputs {
 
 void clean_file(const char* filename); 
 
-pthread_t write_line_to_file_async(char* filename, char* line);
+void write_line_to_file_async(pthread_t* thread, char* filename, char* line);
 
 void* write_line_to_file(void *args);
 
@@ -21,19 +22,21 @@ int main(int argc, char* argv[]) {
     clean_file(second_out_filename);
     int status;
     int status1;
-    pthread_t thread1 = write_line_to_file_async(first_out_filename, "line\n");
-    pthread_t thread2 = write_line_to_file_async(second_out_filename, "line2\n");
+    pthread_t* thread1 = malloc(sizeof(pthread_t));
+    pthread_t* thread2 = malloc(sizeof(pthread_t));
+    write_line_to_file_async(thread1, first_out_filename, "line\n");
+    write_line_to_file_async(thread2, second_out_filename, "line2\n");
     pthread_join(thread1, (void**)&status);
     pthread_join(thread2, (void**)&status1);
     return 0;
 }
 
-pthread_t write_line_to_file_async(char* filename, char* line) {
-    pthread_t thread;
-    struct thread_inputs in = { line, filename };
-    printf(" { input_line = %s, output_filename =  %s }\n ", in.input_line, in.output_filename);
-    pthread_create(&thread, NULL, write_line_to_file, &in);
-    return thread;
+void write_line_to_file_async(pthread_t* thread, char* filename, char* line) {
+    struct thread_inputs* in = malloc(sizeof(struct thread_inputs));
+    in->input_line = line;
+    in->output_filename = filename;    
+    printf(" { input_line = %s, output_filename =  %s }\n ", in->input_line, in->output_filename);
+    pthread_create(thread, NULL, write_line_to_file, in);
 }
 
 void clean_file(const char* filename) {
