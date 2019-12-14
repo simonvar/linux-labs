65a4e5cddcc66478922e59c0e1a3c4ac36fb73e3
Yaroslav Atroshenko <yaaroslavok@gmail.com>
2019-12-14 12:07:04 +0300

    add even line implementation

--- lab4/lab4_2.c
+++ lab4/lab4_2.c
@@ -13,29 +13,51 @@ void write_line_to_file_async(pthread_t* thread, char* filename, char* line);
 
 void* write_line_to_file(void *args);
 
+char* read_line_from_opened_file(FILE* file);
+
 int main(int argc, char* argv[]) {
     char* source_filename = argv[1];
     char* first_out_filename = argv[2];
-    char* second_out_filename = argv[3];
-    
+    char* second_out_filename = argv[3]; 
+    int status;
+
     clean_file(first_out_filename);
     clean_file(second_out_filename);
-    int status;
-    int status1;
-    pthread_t* thread1 = malloc(sizeof(pthread_t));
-    pthread_t* thread2 = malloc(sizeof(pthread_t));
-    write_line_to_file_async(thread1, first_out_filename, "line\n");
-    write_line_to_file_async(thread2, second_out_filename, "line2\n");
-    pthread_join(thread1, (void**)&status);
-    pthread_join(thread2, (void**)&status1);
+
+    FILE* source_file = fopen(source_filename, "r");
+    char* line;
+    while ((line = read_line_from_opened_file(source_file)) != NULL) {
+        pthread_t* thread1 = malloc(sizeof(pthread_t));
+        write_line_to_file_async(thread1, first_out_filename, line);
+        pthread_join(*thread1, (void**)&status);
+        pthread_t* thread2 = malloc(sizeof(pthread_t));
+	char* line2 = read_line_from_opened_file(source_file);
+	if(line2 == NULL) {
+		break;
+	}
+        write_line_to_file_async(thread2, second_out_filename, line2);
+        pthread_join(*thread2, (void**)&status);
+	free(thread1);
+	free(thread2);
+	free(line);
+	free(line2);
+    }
     return 0;
+}
+
+char* read_line_from_opened_file(FILE* file) {
+    int buffer_size = 255;
+    char* result = malloc(buffer_size);    
+    if(fgets(result, buffer_size, file) == NULL) {
+	return NULL;    
+    }
+    return result;
 }
 
 void write_line_to_file_async(pthread_t* thread, char* filename, char* line) {
     struct thread_inputs* in = malloc(sizeof(struct thread_inputs));
     in->input_line = line;
     in->output_filename = filename;    
-    printf(" { input_line = %s, output_filename =  %s }\n ", in->input_line, in->output_filename);
     pthread_create(thread, NULL, write_line_to_file, in);
 }
 
@@ -46,6 +68,12 @@ void clean_file(const char* filename) {
 
 void* write_line_to_file(void *args) {
     struct thread_inputs* in = (struct thread_inputs*)args;
-    printf(" { input_line = %s, output_filename =  %s }\n ", in->input_line, in->output_filename);
+    printf("input_line: %s\n", in->input_line);
+    printf("output_filename: %ld\n",(long) in->output_filename);
+    FILE* file = fopen(in->output_filename, "a");
+    fprintf(file, "%s", in->input_line);
+    fclose(file);
+    free(args);
     return 0;
 }
+
